589799dcfc377cb47c2df2a6fd946c66bca31ce6
shtseng <q253upng@gmail.com>
2018-04-13 23:00:03 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Turtlebot guide planners 
+[![Build Status](https://travis-ci.org/SHTseng/turtlebot_guide_planners.svg?branch=master)](https://travis-ci.org/SHTseng/turtlebot_guide_planners)
+----
+## Sub-packages
+
+* **global\_planner**
+
+  In the form of move_base global planner plugin, plan a path and find out the turning points
+
+* **teb\_local\_planner**
+
+  Modified based on the original work of teb\_local\_planer
---
+++ teb_local_planner/include/teb_local_planner/g2o_types/edge_following_range.h
@@ -0,0 +1,89 @@
+#ifndef EDGE_FOLLOWING_RANGE_H
+#define EDGE_FOLLOWING_RANGE_H
+
+#include <teb_local_planner/g2o_types/vertex_pose.h>
+#include <teb_local_planner/g2o_types/vertex_timediff.h>
+#include <teb_local_planner/g2o_types/base_teb_edges.h>
+#include <teb_local_planner/g2o_types/penalties.h>
+#include <teb_local_planner/teb_config.h>
+#include <teb_local_planner/robot_footprint_model.h>
+
+namespace teb_local_planner
+{
+
+class EdgeFollowingRange : public BaseTebUnaryEdge<2, const PoseSE2*, VertexPose>
+{
+public:
+
+  EdgeFollowingRange() : t_(0.0)
+  {
+    _measurement = NULL;
+  }
+
+  EdgeFollowingRange(double t) : t_(t){}
+
+  void computeError()
+  {
+    ROS_ASSERT_MSG(cfg_ && _measurement, "You must call setTebConfig(), setViaPoint() on EdgeViaPoint()");
+    const VertexPose* bandpt = static_cast<const VertexPose*>(_vertices[0]);
+
+//    double dist = robot_model_->estimateSpatioTemporalDistance(bandpt->pose(), _measurement, t_);
+//    double dist = hypot(bandpt->pose().x()- (_measurement->position().x()+t_*follower_vel_->position().x()),
+//                        bandpt->pose().y()- (_measurement->position().y()+t_*follower_vel_->position().y()));
+//    double dist = bandpt->pose().x()- (_measurement->position().x()+t_*follower_vel_->position().x());
+    double dist = (bandpt->position() - _measurement->position()).norm();
+//    _error[0] = (bandpt->position() - _measurement->position()).norm();
+
+    //    _error[0] = penaltyBoundToInterval(bandpt->position().x() - _measurement->x(), 1.2, 2.0, cfg_->optim.penalty_epsilon);
+//    _error[1] = penaltyBoundToInterval(bandpt->position().y() - _measurement->y(), -0.3, 0.3, cfg_->optim.penalty_epsilon);
+
+    _error[0] = penaltyBoundFromBelow(dist, cfg_->follower.min_follower_dist, cfg_->optim.penalty_epsilon);
+    _error[1] = penaltyBoundToInterval(dist, cfg_->follower.max_follower_dist, cfg_->optim.penalty_epsilon);
+
+    ROS_ASSERT_MSG(std::isfinite(_error[0]), "EdgeFollowingRange::computeError() _error[0]=%f\n",_error[0]);
+  }
+
+  void setFollowerPose(const PoseSE2* follower_pose)
+  {
+    _measurement = follower_pose;
+  }
+
+  void setFollowerVel(const PoseSE2* follower_vel)
+  {
+    follower_vel_ = follower_vel;
+  }
+
+  /**
+   * @brief Set pointer to the robot model
+   * @param robot_model Robot model required for distance calculation
+   */
+  void setRobotModel(const BaseRobotFootprintModel* robot_model)
+  {
+    robot_model_ = robot_model;
+  }
+
+  /**
+   * @brief Set all parameters at once
+   * @param cfg TebConfig class
+   * @param robot_model Robot model required for distance calculation
+   * @param follower 2D position vector containing the position of the obstacle
+   */
+  void setParameters(const TebConfig& cfg, const BaseRobotFootprintModel* robot_model, const PoseSE2* follower_pose)
+  {
+    cfg_ = &cfg;
+    robot_model_ = robot_model;
+    _measurement = follower_pose;
+  }
+
+protected:
+
+  const PoseSE2* follower_vel_;
+  const BaseRobotFootprintModel* robot_model_; //!< Store pointer to robot_model
+  double t_; //!< Estimated time until current pose is reached
+
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+};
+} // end namespace
+
+#endif // EDGE_FOLLOWING_RANGE_H
